594d5e396fabf0a7169366a79be2843d9ef383d8
maria.swiatkowska <maria.swiatkowska@dynatrace.com>
2024-01-26 12:33:01 +0100

    Added terraform templates for aws role-based authentication

---
+++ aws/role-based-access/terraform-templates/with_existing_ActiveGate/ActiveGate_monitoring_role/role_based_access_AG_account_template.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_iam_role" "ActiveGate_role" {
+  name               = var.ActiveGate_role_name
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.ActiveGate_role_assume_role_policy_document.json
+  managed_policy_arns = [aws_iam_policy.Dynatrace_assume_policy.arn]
+}
+
+data "aws_iam_policy_document" "ActiveGate_role_assume_role_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "Dynatrace_assume_policy" {
+  name = var.assume_policy_name
+  policy = data.aws_iam_policy_document.Dynatrace_assume_policy_document.json
+}
+
+data "aws_iam_policy_document" "Dynatrace_assume_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    resources = [
+      "arn:aws:iam::${var.monitored_account_id}:role/${var.monitoring_role_name}"
+    ]
+  }
+}
+
+resource "aws_iam_instance_profile" "ActiveGate_instance_profile" {
+  name = var.ActiveGate_role_name
+  role = aws_iam_role.ActiveGate_role.name
+}
+
+data "aws_caller_identity" "current" {}
+
+output "ActiveGate_role_name" {
+  value       = aws_iam_role.ActiveGate_role.name
+  description = "IAM role name for the account hosting the ActiveGate"
+}
+
+output "ActiveGate_account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "Your Amazon account ID hosting the ActiveGate"
+}
+
+output "monitoring_role_name" {
+  value       = var.monitoring_role_name
+  description = "IAM role that Dynatrace should use to get monitoring data"
+}
+
+output "monitored_account_id" {
+  value       = var.monitored_account_id
+  description = "ID of the account that Dynatrace should monitor"
+}
---
+++ aws/role-based-access/terraform-templates/without_ActiveGate/role_based_access_without_AG_template.tf
@@ -0,0 +1,146 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_iam_role" "monitoring_role" {
+  name               = var.role_name
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.monitoring_role_policy_document.json
+  managed_policy_arns = [aws_iam_policy.monitoring_policy.arn]
+}
+
+data "aws_iam_policy_document" "monitoring_role_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "509560245411" # Dynatrace monitoring account ID
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values   = [var.external_id]
+    }
+  }
+}
+
+resource "aws_iam_policy" "monitoring_policy" {
+  name = var.policy_name
+  policy = data.aws_iam_policy_document.monitoring_policy_document.json
+}
+
+data "aws_iam_policy_document" "monitoring_policy_document" {
+  statement {
+    sid = "VisualEditor0"
+    actions = [
+        "acm-pca:ListCertificateAuthorities",
+        "apigateway:GET",
+        "apprunner:ListServices",
+        "appstream:DescribeFleets",
+        "appsync:ListGraphqlApis",
+        "athena:ListWorkGroups",
+        "autoscaling:DescribeAutoScalingGroups",
+        "cloudformation:ListStackResources",
+        "cloudfront:ListDistributions",
+        "cloudhsm:DescribeClusters",
+        "cloudsearch:DescribeDomains",
+        "cloudwatch:GetMetricData",
+        "cloudwatch:GetMetricStatistics",
+        "cloudwatch:ListMetrics",
+        "codebuild:ListProjects",
+        "datasync:ListTasks",
+        "dax:DescribeClusters",
+        "directconnect:DescribeConnections",
+        "dms:DescribeReplicationInstances",
+        "dynamodb:ListTables",
+        "dynamodb:ListTagsOfResource",
+        "ec2:DescribeAvailabilityZones",
+        "ec2:DescribeInstances",
+        "ec2:DescribeNatGateways",
+        "ec2:DescribeSpotFleetRequests",
+        "ec2:DescribeTransitGateways",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVpnConnections",
+        "ecs:ListClusters",
+        "eks:ListClusters",
+        "elasticache:DescribeCacheClusters",
+        "elasticbeanstalk:DescribeEnvironmentResources",
+        "elasticbeanstalk:DescribeEnvironments",
+        "elasticfilesystem:DescribeFileSystems",
+        "elasticloadbalancing:DescribeInstanceHealth",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeRules",
+        "elasticloadbalancing:DescribeTags",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticmapreduce:ListClusters",
+        "elastictranscoder:ListPipelines",
+        "es:ListDomainNames",
+        "events:ListEventBuses",
+        "firehose:ListDeliveryStreams",
+        "fsx:DescribeFileSystems",
+        "gamelift:ListFleets",
+        "glue:GetJobs",
+        "inspector:ListAssessmentTemplates",
+        "kafka:ListClusters",
+        "kinesis:ListStreams",
+        "kinesisanalytics:ListApplications",
+        "kinesisvideo:ListStreams",
+        "lambda:ListFunctions",
+        "lambda:ListTags",
+        "lex:GetBots",
+        "logs:DescribeLogGroups",
+        "mediaconnect:ListFlows",
+        "mediaconvert:DescribeEndpoints",
+        "mediapackage-vod:ListPackagingConfigurations",
+        "mediapackage:ListChannels",
+        "mediatailor:ListPlaybackConfigurations",
+        "opsworks:DescribeStacks",
+        "qldb:ListLedgers",
+        "rds:DescribeDBClusters",
+        "rds:DescribeDBInstances",
+        "rds:DescribeEvents",
+        "rds:ListTagsForResource",
+        "redshift:DescribeClusters",
+        "robomaker:ListSimulationJobs",
+        "route53:ListHostedZones",
+        "route53resolver:ListResolverEndpoints",
+        "s3:ListAllMyBuckets",
+        "sagemaker:ListEndpoints",
+        "sns:ListTopics",
+        "sqs:ListQueues",
+        "storagegateway:ListGateways",
+        "sts:GetCallerIdentity",
+        "swf:ListDomains",
+        "tag:GetResources",
+        "tag:GetTagKeys",
+        "transfer:ListServers",
+        "workmail:ListOrganizations",
+        "workspaces:DescribeWorkspaces",
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+output "role_name" {
+  value       = aws_iam_role.monitoring_role.name
+  description = "IAM role that Dynatrace should use to get monitoring data"
+}
+
+output "account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "Your Amazon account ID"
+}
